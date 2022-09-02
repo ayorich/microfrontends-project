ffacc7b0b723202d87705145d67e7a4ef08990d5
ayorich <kayodele17@yahoo.com>
2022-09-02 06:07:40 +0100

    child-to-container-navigation-communication-sync

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './App';
-const mount = (el) => {
-  ReactDom.render(<App />, el);
+const mount = (el, { onNavigate }) => {
+  const history = createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+  ReactDom.render(<App history={history} />, el);
 };
 
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, {});
   }
 }
 
